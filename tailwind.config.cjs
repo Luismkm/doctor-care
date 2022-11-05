4e624e00d59823cbbb108626ad3ac67faff571bf
Luismkm <luismarcelo13@yahoo.com.br>
2022-11-05 20:09:35 -0300

    chore: add theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,30 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    fontFamily: {
-      sans: ['DM sans', 'sans-serif']
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      green: {
+        900: '#00453A',
+        800: '#00856F',
+        400: '#DCE9E2',
+        200: '#F7F9F9',
+        100: '#FFFAF1',
+      },
+      gray:{
+        900: '#212529',
+        700: '#495057',
+      }
+
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['DM sans', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
