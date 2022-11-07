de5b0fecfe5d9c1262e1ffd1a38e753ecd6a56ab
Luismkm <luismarcelo13@yahoo.com.br>
2022-11-06 22:40:09 -0300

    chore: modify tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,11 +9,13 @@ module.exports = {
       transparent: 'transparent',
 
       black: '#000',
-      white: '#fff',
+      white: {
+        100: '#FFF',
+      },
 
       green: {
         900: '#00453A',
-        800: '#00856F',
+        700: '#00856F',
         400: '#DCE9E2',
         200: '#F7F9F9',
         100: '#FFFAF1',
